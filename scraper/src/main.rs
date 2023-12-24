4f0cbade302090ca6fc7255d7998eaf0febb529e
NotNite <hi@notnite.com>
2023-12-23 22:35:32 -0500

    Use config for hosts

--- scraper/src/main.rs
+++ scraper/src/main.rs
@@ -1,4 +1,5 @@
 use manager::Manager;
+use serde::Deserialize;
 use std::sync::Arc;
 use thirtyfour::{DesiredCapabilities, WebDriver};
 use tokio::sync::Mutex;
@@ -7,18 +8,25 @@ mod manager;
 mod types;
 mod worker;
 
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    hosts: Vec<String>,
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let manager = Manager::new();
     let manager = Arc::new(Mutex::new(manager));
     let mut tasks = vec![];
 
-    for i in 0..2 {
-        let port = 4444 + i;
+    let config_path = std::env::args().nth(1).unwrap_or("config.json".to_string());
+    let config = std::fs::read_to_string(config_path)?;
+    let config: Config = serde_json::from_str(&config)?;
+
+    for host in config.hosts {
         let mut caps = DesiredCapabilities::chrome();
         caps.add_chrome_arg(&format!("--user-agent={}", types::USER_AGENT))?;
-
-        let driver = WebDriver::new(&format!("http://localhost:{}", port), caps).await?;
+        let driver = WebDriver::new(&host, caps).await?;
         let worker = worker::Worker::new(manager.clone(), driver);
         tasks.push(worker.run());
     }
