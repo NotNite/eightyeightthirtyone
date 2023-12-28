17f524f64a350c4a589e5e0285260af03bea36ad
Chen Marisa <flleeppyy@gmail.com>
2023-12-28 06:26:09 -0800

    fix: remove thing i forgot to remove

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -35,7 +35,7 @@ export default function App() {
   React.useEffect(() => {
     async function createGraphData() {
       const origGraph: ScrapedGraph = await fetch(
-        "https://eightyeightthirty.one" + "/graph.json?t=" + Date.now()
+        "/graph.json?t=" + Date.now()
       ).then((x) => x.json());
       setOrigGraph(origGraph);
 
